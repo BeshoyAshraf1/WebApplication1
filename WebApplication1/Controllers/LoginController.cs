ac89cd913acfd307beb3f95e0af34f4c91e6f737
GradPro <beshoy257ashraf@gmail.com>
2023-09-05 07:41:16 +0300

    Add project files.

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourWebApiNamespace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        @
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            // Perform authentication logic here
+            if (request.Username == "admin" && request.Password == "password")
+            {
+                // Authentication successful
+                return Ok(new { message = "Login successful" });
+            }
+            else
+            {
+                // Authentication failed
+                return Unauthorized(new { message = "Invalid username or password" });
+            }
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
